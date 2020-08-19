3f6fe77c2bd906bbfe3941c5baa3448b0532d538
seuchoi <seuchoi@gmail.com>
2020-08-19 14:25:39 -0400

    new functions

---
+++ R/assocTestAggregate_collapsed.R
@@ -0,0 +1,282 @@
+setGeneric("assocTestAggregate_v2", function(gdsobj, ...) standardGeneric("assocTestAggregate_v2"))
+
+
+.match.arg <- function(test) {
+    if (length(test) > 1) test <- NULL
+    match.arg(test, choices=c("Collapse","Burden", "SKAT", "fastSKAT", "SMMAT", "fastSMMAT", "SKATO"))
+}
+
+
+setMethod("assocTestAggregate_v2",
+          "SeqVarIterator",
+          function(gdsobj, null.model, AF.max=1,
+                   weight.beta=c(1,1), weight.user=NULL,
+                   test=c("Collapse","Burden", "SKAT", "fastSKAT", "SMMAT", "SKATO"),
+                   # burden.test=c("Score"),
+                   # pval.method=c("davies", "kuonen", "liu"),
+                   neig = 200, ntrace = 500,
+                   rho = seq(from = 0, to = 1, by = 0.1),
+                   sparse=TRUE, imputed=FALSE,
+                   male.diploid=TRUE, genome.build=c("hg19", "hg38"),
+                   verbose=TRUE) {
+
+              # check argument values
+              test <- .match.arg(test)
+              # burden.test <- match.arg(burden.test)
+              # pval.method <- match.arg(pval.method)
+
+              # don't use sparse matrices for imputed dosages
+              if (imputed) sparse <- FALSE
+
+              # coerce null.model if necessary
+              if (sparse) null.model <- GENESIS:::.nullModelAsMatrix(null.model)
+
+              # filter samples to match null model
+              sample.index <- GENESIS:::.setFilterNullModel(gdsobj, null.model, verbose=verbose)
+
+              # do we need to match on alleles?
+              match.alleles <- any(c("ref", "alt") %in% names(mcols(currentRanges(gdsobj))))
+
+              # check ploidy
+              if (SeqVarTools:::.ploidy(gdsobj) == 1) male.diploid <- FALSE
+
+              # results
+              res <- list()
+              res.var <- list()
+              i <- 1
+              n.iter <- length(variantFilter(gdsobj))
+              set.messages <- ceiling(n.iter / 100) # max messages = 100
+              iterate <- TRUE
+              while (iterate) {
+                  var.info <- variantInfo(gdsobj, alleles=match.alleles, expanded=TRUE)
+
+                  if (!imputed) {
+                      geno <- expandedAltDosage(gdsobj, use.names=FALSE, sparse=sparse)[sample.index,,drop=FALSE]
+                  } else {
+                      geno <- imputedDosage(gdsobj, use.names=FALSE)[sample.index,,drop=FALSE]
+                  }
+
+                  if (match.alleles) {
+                      index <- GENESIS:::.matchAlleles(gdsobj, var.info)
+                      var.info <- var.info[index,,drop=FALSE]
+                      geno <- geno[,index,drop=FALSE]
+                  } else {
+                      index <- NULL
+                  }
+
+                  # number of non-missing samples
+                  # n.obs <- colSums(!is.na(geno))
+                  n.obs <- GENESIS:::.countNonMissing(geno, MARGIN = 2)
+
+                  # allele frequency
+                  freq <- GENESIS:::.alleleFreq(gdsobj, geno, variant.index=index, sample.index=sample.index,
+                                      male.diploid=male.diploid, genome.build=genome.build)
+
+                  # filter monomorphic variants
+                  keep <- GENESIS:::.filterMonomorphic(geno, count=n.obs, freq=freq$freq, imputed=imputed)
+
+                  # exclude variants with freq > max
+                  keep <-  keep & freq$freq <= AF.max
+                  if (!all(keep)) {
+                      var.info <- var.info[keep,,drop=FALSE]
+                      geno <- geno[,keep,drop=FALSE]
+                      n.obs <- n.obs[keep]
+                      freq <- freq[keep,,drop=FALSE]
+                  }
+
+                  # weights
+                  if (is.null(weight.user)) {
+                      # Beta weights
+                      weight <- GENESIS:::.weightFromFreq(freq$freq, weight.beta)
+                  } else {
+                      # user supplied weights
+                      weight <- currentVariants(gdsobj)[[weight.user]][expandedVariantIndex(gdsobj)]
+                      if (!is.null(index)) weight <- weight[index]
+                      weight <- weight[keep]
+
+                      weight0 <- is.na(weight) | weight == 0
+                      if (any(weight0)) {
+                          keep <- !weight0
+                          var.info <- var.info[keep,,drop=FALSE]
+                          geno <- geno[,keep,drop=FALSE]
+                          n.obs <- n.obs[keep]
+                          freq <- freq[keep,,drop=FALSE]
+                          weight <- weight[keep]
+                      }
+                  }
+
+                  # number of variant sites
+                  n.site <- length(unique(var.info$variant.id))
+
+                  # number of alternate alleles
+                  n.alt <- sum(geno, na.rm=TRUE)
+
+                  # number of samples with observed alternate alleles > 0
+                  n.sample.alt <- sum(rowSums(geno, na.rm=TRUE) > 0)
+
+                  res[[i]] <- data.frame(n.site, n.alt, n.sample.alt)
+                  res.var[[i]] <- cbind(var.info, n.obs, freq, weight)
+
+                  if (n.site > 0) {
+                      # zero impute missing values
+                    if (any(n.obs < nrow(geno)) & test=="Collapse") {
+                        geno <- zeroImpute_Sean(geno, freq$freq)
+                    }else if(any(n.obs < nrow(geno)) & test!="Collapse") {
+                      # mean impute missing values
+                        geno <- GENESIS:::.meanImpute(geno, freq$freq)
+                      }
+
+                      # do the test
+                      assoc <- testVariantSet(null.model, G=geno, weights=weight,
+                                              test=test, # burden.test=burden.test,
+                                              neig = neig, ntrace = ntrace,
+                                              rho=rho)
+                                              # pval.method=pval.method)
+                      res[[i]] <- cbind(res[[i]], assoc, stringsAsFactors=FALSE)
+                  }
+
+                  if (verbose & n.iter > 1 & i %% set.messages == 0) {
+                      message(paste("Iteration", i , "of", n.iter, "completed"))
+                  }
+                  i <- i + 1
+                  iterate <- iterateFilter(gdsobj, verbose=FALSE)
+              }
+
+              res <- list(results=dplyr::bind_rows(res), variantInfo=res.var)
+              GENESIS:::.annotateAssoc(gdsobj, res)
+          })
+
+
+#
+zeroImpute_Sean <- function(geno, freq) {
+    miss.idx <- Matrix::which(is.na(geno))
+    miss.var.idx <- ceiling(miss.idx/nrow(geno))
+    geno[miss.idx] <- 0 * freq[miss.var.idx]
+    geno
+}
+
+
+testVariantSet <- function( nullmod, G, weights,
+                                      test = c("Collapse","Burden", "SKAT", "fastSKAT", "SMMAT", "fastSMMAT", "SKATO"),
+                                      # burden.test = c("Score"),
+                                      neig = 200, ntrace = 500,
+                                      rho = seq(from = 0, to = 1, by = 0.1)){
+                                     # pval.method = c("davies", "kuonen", "liu"),
+                                     # return.scores = FALSE, return.scores.cov = FALSE){
+
+              test <- match.arg(test)
+              # burden.test <- match.arg(burden.test)
+              # pval.method <- match.arg(pval.method)
+
+              G <- GENESIS:::.genoAsMatrix(nullmod, G)
+              if (test == "Collapse") {
+                  out <- .testVariantSetCollapse(nullmod, G, weights, burden.test = "Score")
+              }
+              if (test == "Burden") {
+                  out <- GENESIS:::.testVariantSetBurden(nullmod, G, weights, burden.test = "Score")
+              }
+              if (test == "SKAT") {
+                  out <- GENESIS:::.testVariantSetSKAT(nullmod, G, weights, neig = Inf, ntrace = Inf)
+                                             # return.scores, return.scores.cov)
+              }
+              if(test == "fastSKAT"){
+                  out <- GENESIS:::.testVariantSetSKAT(nullmod, G, weights, neig, ntrace)
+              }
+              if (test == "SMMAT") {
+                  out <- GENESIS:::.testVariantSetSMMAT(nullmod, G, weights, neig = Inf, ntrace = Inf)
+              }
+              if(test == "fastSMMAT"){
+                  out <- GENESIS:::.testVariantSetSMMAT(nullmod, G, weights, neig, ntrace)
+              }
+              if(test == "SKATO"){
+                  out <- GENESIS:::.testVariantSetSKATO(nullmod, G, weights, rho)
+              }
+              return(out)
+          }
+
+
+
+          ## create the burden score, than calls the appropriate single variant test function.
+          ## can easily implement GxE interaction with the burden score... later!
+.testVariantSetCollapse <- function(nullmod, G, weights, burden.test){
+              # multiply G by weights and compute burden
+              if(is(G, "Matrix")){
+                  burden <- rowSums(G %*% Diagonal(x = weights))
+              }else{
+                  burden <- colSums(t(G) * weights)
+              }
+              burden<-ifelse(burden>=1,1,0)
+              # adjust burden for covariates and random effects
+              Gtilde <- GENESIS:::calcGtilde(nullmod, burden)
+              if(is.null(nullmod$RSS0)){
+                  nullmod$RSS0 <- as.numeric(crossprod(nullmod$Ytilde))
+              }
+
+              if (burden.test == "Score") {
+                  out <- GENESIS:::.testGenoSingleVarScore(Gtilde, G = burden, resid = nullmod$resid, RSS0 = nullmod$RSS0)
+              }
+              # if (burden.test == "Wald"){
+              #     out <- .testGenoSingleVarWald(Gtilde, Ytilde = nullmod$Ytilde,
+              #                                   n = length(nullmod$Ytilde), k = ncol(nullmod$model.matrix))
+              # }
+              return(out)
+          }
+
+####
+##### function start
+aggreated_test<-function(num,gdsfile,varfile,groupfile,phenfile,nullfile,cutoff,stat,outfile){
+
+##### samples
+phen1<-fread(phenfile,header=T,data.table=F,sep="\t")
+names(phen1)[1]<-"sample.id"
+samid0<-phen1$sample.id
+
+######
+###### QCed variants
+vardata<-fread(varfile,header=F,sep="\t",data.table=F)
+varid0<-vardata$V1
+
+######
+###### read gds file
+gds <- seqOpen(gdsfile, allow.duplicate=T)
+samples <- seqGetData(gds, "sample.id")
+missamples<-samples[!samples %in% samid0]
+misphen<-data.frame(matrix(NA,nrow=length(missamples),ncol=ncol(phen1)))
+colnames(misphen)<-names(phen1)
+misphen$sample.id<-missamples
+combphen<-rbind(phen1,misphen)
+rownames(combphen)<-combphen$sample.id
+combphen2<-combphen[samples,]
+
+######
+###### # construct a SeqVarData object
+seqData <- SeqVarData(gds, sampleData=AnnotatedDataFrame(combphen2))
+
+######
+###### filter the gdsfile
+seqSetFilter(seqData, sample.id=samid0, variant.id=varid0)
+
+######
+###### annotation file
+annot<-get(load(groupfile))
+annot$pos<-as.numeric(annot$pos)
+######
+###### grouping file
+gr<-aggregateGRangesList(annot)
+
+######
+###### create the iterator
+iterator <- SeqVarListIterator(seqData, variantRanges=gr)
+
+######
+###### load null model
+nullmod<-get(load(nullfile))
+
+###### perfrom assocation test
+assoc <- assocTestAggregate_v2(iterator, nullmod, AF.max=cutoff, test=stat,verbose=TRUE)
+
+#####
+save(assoc,file=outfile)
+#####
+seqClose(gds)
+}
---
+++ R/delscore.R
@@ -0,0 +1,117 @@
+
+delscore<-function(filename,mintool=7){
+library(data.table)
+library(tidyr)
+
+dat0<-fread(cmd=paste0("zcat ",filename),header=T,data.table=F,sep="\t")
+vars<-c("SIFT_pred","SIFT4G_pred","Polyphen2_HDIV_pred","Polyphen2_HVAR_pred","LRT_pred","MutationTaster_pred","MutationAssessor_pred","FATHMM_pred","PROVEAN_pred","VEST4_rankscore","MetaSVM_pred","MetaLR_pred","M-CAP_pred","REVEL_rankscore","MutPred_rankscore","MVP_rankscore","MPC_rankscore","PrimateAI_pred","DEOGEN2_pred","BayesDel_addAF_pred","BayesDel_noAF_pred","ClinPred_pred","LIST-S2_pred","Aloft_pred","Aloft_Confidence","CADD_phred","DANN_rankscore","fathmm-MKL_coding_pred","fathmm-XF_coding_pred","Eigen-phred_coding","Eigen-PC-phred_coding")
+
+
+##########
+##########
+
+dselect<-c("SIFT_pred","SIFT4G_pred","Polyphen2_HDIV_pred","Polyphen2_HVAR_pred","LRT_pred","MutationTaster_pred","FATHMM_pred","PROVEAN_pred","MetaSVM_pred","MetaLR_pred","M-CAP_pred","PrimateAI_pred","DEOGEN2_pred","BayesDel_addAF_pred","BayesDel_noAF_pred","ClinPred_pred","LIST-S2_pred","fathmm-MKL_coding_pred","fathmm-XF_coding_pred")
+
+for (ii in 1:length(dselect)){
+variable0<-dselect[ii]
+table(dat0[,variable0])
+newvar<-paste0(variable0,"_D")
+dat0[,newvar]<-NA
+dat0[grep("T|P|B|N|U",dat0[,variable0]),newvar]<-0
+dat0[grep("D",dat0[,variable0],fixed=T),newvar]<-1
+##print(table(dat0[,newvar],useNA="always"))
+}
+##########
+##########
+hselect<-c("MutationAssessor_pred")
+
+for (ii in 1:length(hselect)){
+variable0<-hselect[ii]
+##table(dat0[,variable0])
+newvar<-paste0(variable0,"_D")
+dat0[,newvar]<-NA
+dat0[grep("M|L|N",dat0[,variable0]),newvar]<-0
+dat0[grep("H",dat0[,variable0],fixed=T),newvar]<-1
+##print(table(dat0[,newvar],useNA="always"))
+}
+
+#########
+######### ranks
+rank<-c("VEST4_rankscore","REVEL_rankscore","MutPred_rankscore","MVP_rankscore","MPC_rankscore","DANN_rankscore")
+
+for (ii in 1:length(rank)){
+variable0<-rank[ii]
+newvar<-paste0(variable0,"_D")
+dat0[,newvar]<-NA
+dat0[,variable0]<-as.numeric(dat0[,variable0])
+dat0[which(dat0[,variable0]<0.9),newvar]<-0
+dat0[which(dat0[,variable0]>=0.9),newvar]<-1
+#print(table(dat0[,newvar],useNA="always"))
+}
+
+########
+######## phred scale
+phred<-c("CADD_phred","Eigen-phred_coding","Eigen-PC-phred_coding")
+
+for (ii in 1:length(phred)){
+variable0<-phred[ii]
+#summary(dat0[,variable0])
+newvar<-paste0(variable0,"_D")
+dat0[,newvar]<-NA
+dat0[,variable0]<-as.numeric(dat0[,variable0])
+#summary(dat0[,variable0])
+dat0[which(dat0[,variable0]<10),newvar]<-0
+dat0[which(dat0[,variable0]>=10),newvar]<-1
+#print(table(dat0[,newvar],useNA="always"))
+}
+
+########
+######## aloft is difficult
+aloft<-c("Aloft_pred","Aloft_Confidence")
+variable0<-aloft[1]
+#table(dat0[,variable0])
+
+newvar<-paste0(variable0,"_D")
+dat0$taloft<-gsub("Tolerant","T",gsub("Recessive","D",gsub("Dominant","D",dat0[,aloft[1]])))
+dat0$taloftconf<-gsub("Low Confidence","L",gsub("High Confidence","D",dat0[,aloft[2]]))
+dat0[,newvar]<-NA
+dat0[grep("T|D",dat0[,variable0]),newvar]<-0
+
+test<-dat0[grep("D",dat0$taloft),]
+for (ii in 1:nrow(test)){
+trow<-test[ii,]
+dlocdel<-grep("D",unlist(strsplit(trow$taloft,",")))
+dlocconf<-grep("D",unlist(strsplit(trow$taloftconf,",")))
+test[ii,newvar]<-ifelse(sum(dlocdel %in% dlocconf)>=1,1,test[ii,newvar])
+}
+
+dat0[rownames(test),newvar]<-test[,newvar]
+#print(table(dat0[rownames(test),"taloftconf"],dat0[rownames(test),newvar]))
+#print(table(dat0[,newvar],useNA="always"))
+
+###########
+########### create score
+dvars<-paste0(vars[!vars %in% "Aloft_Confidence"],"_D")
+dat1<-dat0[,dvars]
+dat1$Dscore<-apply(dat1[,dvars],1,sum,na.rm=T)
+dat1$Dnmiss<-apply(dat1[,dvars],1,function(x){sum(!is.na(x))})
+dat1$Dprop<-apply(dat1[,dvars],1,function(x){mean(x,na.rm=T)})
+
+###########
+########### add the score to data
+dat0$Dscore<-dat1$Dscore
+dat0$Dtools<-dat1$Dnmiss
+dat0$Dprop<-dat1$Dprop
+
+##########
+dat2<-subset(dat0,Dtools>=mintool)
+dat2$gvarID<-paste0(dat2[,1],":",dat2[,4])
+dat3<-dat2[order(dat2$gvarID,-dat2$Dprop),]
+dat4<-dat3[!duplicated(dat3$gvarID),]
+dat5<-dat4[,c("gvarID","Dscore","Dtools","Dprop")]
+########## var deleterious info
+
+dat5<-separate(data=dat5,col="gvarID",into=c("chr","pos","ref","alt","geneID"))
+#write.table(dat5,outfile,col.names=T,row.names=F,quote=F,sep="\t")
+return(dat5)
+}
