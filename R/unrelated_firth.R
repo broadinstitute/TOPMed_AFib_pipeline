3da4ab30e9638d5e0a799eb4ab1893dba9a06568
seuchoi <seuchoi@gmail.com>
2020-09-16 15:58:46 -0400

    add generalizable options

--- R/unrelated_firth.R
+++ R/unrelated_firth.R
@@ -1,7 +1,7 @@
 ######## logitf
 library(logistf)
 library(data.table)
-unrelated_firth<-function(phenfile,num,geneid,gdsfile,resultfile){
+unrelated_firth<-function(phenfile,num,geneid,gdsfile,resultfile,unrelcol="unrel_3degree",noHF_col="HF_before_AF"){
 
 cars<-carrier_info(phenfile=phenfile,num=num,gdsfile=gdsfile,geneid=geneid,resultfile=resultfile)
 
@@ -14,8 +14,8 @@ pheno$AF<-ifelse(pheno$AF_status=="case",1,0)
 
 ######
 ###### unrel
-unrel<-subset(pheno,unrel_3degree==1 )
-unrel<-subset(unrel,HF_before_AF==0)
+unrel<-subset(pheno,pheno[,unrelcol]==1 )
+unrel<-subset(unrel,pheno[,noHF_col]==0)
 
 covs<-c("Sex","PC1","PC2","PC4","PC5","PC6","PC8","PC10","PC11")
 form1<-formula(paste0("AF ~ carriers +",paste0(covs,collapse="+")))
