75a9593a7d2fbd0326ff610c964e7204583af155
seuchoi <seuchoi@gmail.com>
2020-09-18 00:52:04 -0400

    update

---
+++ GENESIS/firth/Firth_logistic_regression_unrelated_noHF_before_AF.R
@@ -0,0 +1,30 @@
+####
+#### LOF carrier status
+files.sources = list.files(path="TOPMed_AFib_pipeline/R/")
+files.sources = paste0("TOPMed_AFib_pipeline/R/",files.sources)
+sapply(files.sources, source)
+
+##### argument : chromosome info
+args=(commandArgs(TRUE))
+phenfile=as.character(args[1])
+num=as.character(args[2])
+gdsfile=as.character(args[3])
+genefile=as.character(args[4])
+resultfile=as.character(args[5])
+outfile=as.character(args[6])
+
+####
+#### loop by the geneid
+genelist<-fread(genefile,header=F,sep="\t",data.table=F)
+chrgenes<-subset(genelist,chr==num)
+geneids<-chrgenes$geneid
+
+result<-list()
+for (gnum in 1:length(geneids)){
+geneid<-geneids[gnum]
+
+#### perfrom the firth
+mod1<-unrelated_firth(phenfile=phenfile,num=num,gdsfile=gdsfile,geneid=geneid,resultfile=resultfile)
+result[[geneid]]<-mod1
+}
+save(result,filename=outfile)
