ba5f9e57ae79b6e3a37ac9224419db77b5716d26
seuchoi <seuchoi@gmail.com>
2020-09-22 13:10:30 -0400

    fix the typo

--- R/unrelated_firth.R
+++ R/unrelated_firth.R
@@ -15,7 +15,7 @@ pheno$AF<-ifelse(pheno$AF_status=="case",1,0)
 ######
 ###### unrel
 unrel0<-subset(pheno,pheno[,unrelcol]==1)
-unrel1<-subset(unrel,pheno[,noHF_col]==0)
+unrel1<-subset(unrel0,pheno[,noHF_col]==0)
 
 covs<-c("Sex","PC1","PC2","PC4","PC5","PC6","PC8","PC10","PC11")
 form1<-formula(paste0("AF ~ carriers +",paste0(covs,collapse="+")))
