41c1cfdd5a4ebe9f3374d7e8a4a3c4616129d099
seuchoi <seuchoi@gmail.com>
2020-09-22 12:44:58 -0400

    add tow models

--- R/unrelated_firth.R
+++ R/unrelated_firth.R
@@ -14,12 +14,16 @@ pheno$AF<-ifelse(pheno$AF_status=="case",1,0)
 
 ######
 ###### unrel
-unrel<-subset(pheno,pheno[,unrelcol]==1 )
-unrel<-subset(unrel,pheno[,noHF_col]==0)
+unrel0<-subset(pheno,pheno[,unrelcol]==1)
+unrel1<-subset(unrel,pheno[,noHF_col]==0)
 
 covs<-c("Sex","PC1","PC2","PC4","PC5","PC6","PC8","PC10","PC11")
 form1<-formula(paste0("AF ~ carriers +",paste0(covs,collapse="+")))
-mod1<-logistf(form1,data=unrel)
+mod1<-logistf(form1,data=unrel0)
 
-return(mod1)
+form2<-formula(paste0("AF ~ carriers +",paste0(covs,collapse="+")))
+mod2<-logistf(form1,data=unrel1)
+
+res<-list(unrelated=mod1,unrelated_noHF_before_AF=mod2)
+return(res)
 }
