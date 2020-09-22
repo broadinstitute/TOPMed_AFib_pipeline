249466e69c5c8185c5111bc32fd02227a1f22804
seuchoi <seuchoi@gmail.com>
2020-09-22 12:46:28 -0400

    update for two model running

--- R/unrelated_firth.R
+++ R/unrelated_firth.R
@@ -19,9 +19,11 @@ unrel1<-subset(unrel,pheno[,noHF_col]==0)
 
 covs<-c("Sex","PC1","PC2","PC4","PC5","PC6","PC8","PC10","PC11")
 form1<-formula(paste0("AF ~ carriers +",paste0(covs,collapse="+")))
+
+###### unrelated samples
 mod1<-logistf(form1,data=unrel0)
 
-form2<-formula(paste0("AF ~ carriers +",paste0(covs,collapse="+")))
+###### unrelated and no HF before AF samples
 mod2<-logistf(form1,data=unrel1)
 
 res<-list(unrelated=mod1,unrelated_noHF_before_AF=mod2)
