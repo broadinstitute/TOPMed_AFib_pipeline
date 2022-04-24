818479c7c907fbb10aac9e66ed964f082905bd02
seuchoi <seuchoi@gmail.com>
2022-04-24 08:40:17 -0400

    update new frame

---
+++ DNANexus/kernell_variance_component.R
@@ -0,0 +1,32 @@
+#!/usr/bin/env Rscript
+
+#### binary traits
+args=(commandArgs(TRUE))
+gdsfile=as.character(args[1])
+groupfile=as.character(args[2])
+phenfile=as.character(args[3])
+ID_col=as.numeric(args[4])
+nullfile=as.character(args[5])
+outfile=as.character(args[6])
+
+#########################################################
+#### RUN hclof_noflag_missense0.9 ExtractKernelStatistics
+#########################################################
+
+cat('\nReading in packages for analysis...\n')
+### library(GENESIS)
+### library(data.table)
+### library(SeqArray)
+### library(SeqVarTools)
+
+
+#git clone --branch v1.2 https://github.com/seanjosephjurgens/UKBB_200KWES_CVD.git
+#git pull --branch v1.2 https://github.com/seanjosephjurgens/UKBB_200KWES_CVD.git
+#source("/medpop/afib/sjurgens/Rscripts/association_source_v2.R")
+
+source("UKBB_200KWES_CVD/GENESIS_adaptation_source.R")
+
+kernell_variance_component(gdsfile=gdsfile,groupfile=groupfile,phenfile=phenfile,ID_col=ID_col,nullfile=nullfile,outfile=outfile, test="ExtractKernelStatistics", vc.test="Score", AF.max=0.001, MAC.max=Inf,use.weights=FALSE)
+
+sessionInfo()
+quit("no")
