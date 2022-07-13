d3c09a7902b321b0176e3494fb653a6b49014eaa
seanjosephjurgens <66628479+seanjosephjurgens@users.noreply.github.com>
2022-07-13 12:52:18 +0200

    Update cauchy_make_groupingfile.R

--- DNANexus/Cauchy_pipeline_v2/cauchy_make_groupingfile.R
+++ DNANexus/Cauchy_pipeline_v2/cauchy_make_groupingfile.R
@@ -20,60 +20,89 @@ source("UKBB_200KWES_CVD/Cauchy_test.R")
 
 library(data.table)
 load(paste0(lof_annotfile))
-
 #head(group)
-lof <- group[group$group_id==genename, ]
-#dim(lof)
-lof$varid <- paste0(lof$chr, ":", lof$pos, ":", lof$ref, ":", lof$alt)
-#head(lof)
-#table(lof$TranscriptID)
+if(genename!="ALL"){
+    lof_tot <- group[group$group_id==genename, ]
+    #dim(lof)
+    lof_tot$varid <- paste0(lof_tot$chr, ":", lof_tot$pos, ":", lof_tot$ref, ":", lof_tot$alt)
+    #head(lof)
+    #table(lof$TranscriptID)
+}else{
+    lof_tot <- group  
+}
 
 load(paste0(missense_annotfile))
-#head(group)
-missense <- group[group$group_id==genename & group$Dtools>=7, ]
-#dim(missense)
-missense$varid <- paste0(missense$chr, ":", missense$pos, ":", missense$ref, ":", missense$alt)
-#table(missense$TranscriptID)
+if(genename!="ALL"){
+    #head(group)
+    missense_tot <- group[group$group_id==genename & group$Dtools>=7, ]
+    #dim(missense)
+    missense_tot$varid <- paste0(missense_tot$chr, ":", missense_tot$pos, ":", missense_tot$ref, ":", missense_tot$alt)
+    #table(missense$TranscriptID)
+}else{
+    missense_tot <- group
+}
 
-missense_cutoffs <- c(0.8, 0.6, 0.4, 0.2, 0)
-frequency_cutoffs <- c(0.001, 1e-5, 0)
+missense_cutoffs <- c(0.8, 0.6, 0.4, 0.2)
+frequency_cutoffs <- c(0.001, 1e-5)
 
-for(frequency_cutoff in frequency_cutoffs){
-    rez_group <- NULL
-    for(transcripts in c("ALL", "CANONICAL", c(unique(missense$TranscriptID)))){
-        if(transcripts=="CANONICAL"){
-            inter_missense1 <- missense[missense$CANONICAL=="YES" & missense$gnomad_POPMAX <= frequency_cutoff, ]
-            inter_lof <- lof[lof$CANONICAL=="YES" & lof$gnomad_POPMAX <= frequency_cutoff, ]
-        }else if(transcripts=="ALL"){
-            inter_lof <- lof[lof$gnomad_POPMAX <= frequency_cutoff, ]
-            inter_missense1 <- missense[missense$gnomad_POPMAX <= frequency_cutoff, ]
-            inter_missense1 <- inter_missense1[-(which(duplicated(inter_missense1$varid))), ]
-        }else{
-            inter_missense1 <- missense[missense$CANONICAL!="YES" & missense$TranscriptID==transcripts & missense$gnomad_POPMAX <= frequency_cutoff, ]
-            inter_lof <- lof[lof$CANONICAL!="YES" & lof$TranscriptID==transcripts & lof$gnomad_POPMAX <= frequency_cutoff, ]
-        }
-        for(missense_cutoff in missense_cutoffs){
-            inter_missense <- inter_missense1[inter_missense1$Dprop >= missense_cutoff, ]
-            inter_lof_missense <- rbind(inter_lof, inter_missense)
-            if(nrow(inter_lof_missense)>0){
-                inter_lof_missense$group_id <- paste0(inter_lof_missense$group_id, "_", transcripts, "_hclofmissense", missense_cutoff, "_freq", frequency_cutoff)
+install.packages('foreach')
+install.packages('doParallel')
+library(foreach)
+library(doParallel)
+
+cores=detectCores()
+#cl <- makeCluster(cores[1]-1) #not to overload your computer
+cl <- makeCluster(cores[1]-2)
+registerDoParallel(cl)
+
+paths <- .libPaths()
+genes = unique(group$group_id)
+
+for(frequency_cutoff in frequency_cutoffs){    
+    rez_group <- foreach(gene=genes, .inorder=FALSE, .combine='rbind') %dopar% {
+        .libPaths(paths)
+        source("/opt/notebooks/tmp/UKBB_200KWES_CVD/GENESIS_adaptation_source.R")
+        
+        lof <- lof_tot[lof_tot$group_id==gene, ]
+        missense <- missense_tot[missense_tot$group_id==gene, ]
+        for(transcripts in c("ALL", "CANONICAL", c(unique(missense$TranscriptID)))){
+            if(transcripts=="CANONICAL"){
+                inter_missense1 <- missense[missense$CANONICAL=="YES" & missense$gnomad_POPMAX <= frequency_cutoff, ]
+                inter_lof <- lof[lof$CANONICAL=="YES" & lof$gnomad_POPMAX <= frequency_cutoff, ]
+            }else if(transcripts=="ALL"){
+                inter_lof <- lof[lof$gnomad_POPMAX <= frequency_cutoff, ]
+                inter_missense1 <- missense[missense$gnomad_POPMAX <= frequency_cutoff, ]
+                inter_missense1 <- inter_missense1[-(which(duplicated(inter_missense1$varid))), ]
+            }else{
+                inter_missense1 <- missense[missense$CANONICAL!="YES" & missense$TranscriptID==transcripts & missense$gnomad_POPMAX <= frequency_cutoff, ]
+                inter_lof <- lof[lof$CANONICAL!="YES" & lof$TranscriptID==transcripts & lof$gnomad_POPMAX <= frequency_cutoff, ]
             }
-            if(nrow(inter_missense)>0){
-                inter_missense$group_id <- paste0(inter_missense$group_id, "_", transcripts, "_missense", missense_cutoff, "_freq", frequency_cutoff)
+            for(missense_cutoff in missense_cutoffs){
+                inter_missense <- inter_missense1[inter_missense1$Dprop >= missense_cutoff, ]
+                inter_lof_missense <- rbind(inter_lof, inter_missense)
+                if(nrow(inter_lof_missense)>0){
+                    inter_lof_missense$group_id <- paste0(inter_lof_missense$group_id, "_", transcripts, "_hclofmissense", missense_cutoff, "_freq", frequency_cutoff)
+                }
+                if(nrow(inter_missense)>0){
+                    inter_missense$group_id <- paste0(inter_missense$group_id, "_", transcripts, "_missense", missense_cutoff, "_freq", frequency_cutoff)
+                }
+                rm <- which(duplicated(inter_lof_missense$varid))
+                if(length(rm)>0){inter_lof_missense <- inter_lof_missense[-rm, ]}
+                rm <- which(duplicated(inter_missense$varid))
+                if(length(rm)>0){inter_missense <- inter_missense[-rm, ]}
+                rez_group <- rbind(rez_group, inter_lof_missense, inter_missense)
             }
-            rm <- which(duplicated(inter_lof_missense$varid))
-            if(length(rm)>0){inter_lof_missense <- inter_lof_missense[-rm, ]}
-            rm <- which(duplicated(inter_missense$varid))
-            if(length(rm)>0){inter_missense <- inter_missense[-rm, ]}
-            rez_group <- rbind(rez_group, inter_lof_missense, inter_missense)
+            if(nrow(inter_lof)>0){
+                inter_lof$group_id <- paste0(inter_lof$group_id, "_", transcripts, "_hclof_freq", frequency_cutoff)
+            }
+            rm <- which(duplicated(inter_lof$varid))
+            if(length(rm)>0){inter_lof <- inter_lof[-rm, ]}
+            rez_group <- rbind(rez_group, inter_lof)
         }
-        if(nrow(inter_lof)>0){
-            inter_lof$group_id <- paste0(inter_lof$group_id, "_", transcripts, "_hclof_freq", frequency_cutoff)
-        }
-        rm <- which(duplicated(inter_lof$varid))
-        if(length(rm)>0){inter_lof <- inter_lof[-rm, ]}
-        rez_group <- rbind(rez_group, inter_lof)
     }
     group <- rez_group
     save(group, file=paste0(genename, '_multiple_groupingfile_v1_freq', frequency_cutoff, '.RData'))
 }
+
+
+
