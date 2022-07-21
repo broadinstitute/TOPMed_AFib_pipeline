e872df695e362084a91ae0e5652a211f76bd7cb5
seanjosephjurgens <66628479+seanjosephjurgens@users.noreply.github.com>
2022-07-21 15:45:44 +0200

    Update extract_carriers.R

--- DNANexus/extract_carriers.R
+++ DNANexus/extract_carriers.R
@@ -8,7 +8,7 @@ plink_path=as.character(args[3])
 collapse=as.logical(args[4])
 canonical=as.logical(args[5])
 outfile=as.character(args[6])
-max_maf=as.numeric(args[7])
+max_maf=as.character(args[7])
 
 ##########################################################
 #### RUN extraction
