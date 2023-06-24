b9a31e8e6bdb5756920cb2707ec0bdc8f86a8e61
seanjosephjurgens <66628479+seanjosephjurgens@users.noreply.github.com>
2023-06-24 22:03:15 +0200

    Update collect_results_with_round23.R

--- DNANexus/PheWAS/collect_results_with_round23.R
+++ DNANexus/PheWAS/collect_results_with_round23.R
@@ -509,7 +509,7 @@ if(!(all(file.exists(maf0.001_files)) & all(file.exists(maf0.00001_files)) & all
             regenie_annotationfile <- group[,c("varid", "group_id", "pseudo_annot")]
             set_inter <- NULL
             for(gr in unique(group$group_id)){
-                 gr_inter <- group[group$group_id==gr, ]
+                gr_inter <- group[group$group_id==gr, ]
                 chromosome <- gr_inter[1,'chr']
                 position <- gr_inter[1,'pos']
                 collapse <- paste(gr_inter$varid, collapse=",")
@@ -519,6 +519,8 @@ if(!(all(file.exists(maf0.001_files)) & all(file.exists(maf0.00001_files)) & all
             regenie_setfile <- set_inter
       
             ## Use PLINK2 to filter to a PLINK file that has the needed variants and samples used in the discovery analysis, so also correct MAF filters can be applied!
+            rm <- which(group$varid == "chr9:109080945:T:C") # error variant
+            if(length(rm)>0){group <- group[-rm, ]}
             write.table(group$varid, file=paste0(num, '__varz_chr', chr, '.tsv'), col.names=F, row.names=F, quote=F)
             try(system(paste0(plink_path, ' ',
                 '--', plinkfile_type, '  ', plinkfile, '  ',
