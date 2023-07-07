a8c0c51119c03da8e2458bf2f2217d2eb25d1ff7
Seung Hoan Choi <seuchoi@gmail.com>
2023-07-07 15:36:54 -0400

    update following

--- DNANexus/kernell_variance_component_v3.R
+++ DNANexus/kernell_variance_component_v3.R
@@ -8,6 +8,8 @@ phenfile=as.character(args[3])
 ID_col=as.character(args[4])
 nullfile=as.character(args[5])
 outfile=as.character(args[6])
+testtype=as.character(args[7])
+afcutoff=as.numeric(args[8])
 
 ##########################################################
 #### RUN hclof_noflag_missense0.9 ExtractKernelStatistics
@@ -29,7 +31,7 @@ source("UKBB_200KWES_CVD/GENESIS_adaptation_source.R")
 source("TOPMed_AFib_pipeline/DNANexus/kernell_variance_component_modfied.R")
 source("TOPMed_AFib_pipeline/DNANexus/ExtractKernelStatistics_error_fixed.R")
 
-kernell_variance_component_v3(gdsfile=gdsfile,groupfile=groupfile,phenfile=phenfile,ID_col=ID_col,nullfile=nullfile,outfile=outfile, test="ExtractKernelStatistics", vc.test="Score", AF.max=0.001, MAC.max=Inf,use.weights=FALSE)
+kernell_variance_component_v3(gdsfile=gdsfile,groupfile=groupfile,phenfile=phenfile,ID_col=ID_col,nullfile=nullfile,outfile=outfile, test="ExtractKernelStatistics", vc.test=testtype, AF.max=afcutoff, MAC.max=Inf,use.weights=FALSE)
 
 sessionInfo()
 quit("no")
