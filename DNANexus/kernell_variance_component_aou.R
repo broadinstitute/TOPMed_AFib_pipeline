8592f8f6705f6e62af77cd375b498c6f02c24660
Seung Hoan Choi <seuchoi@gmail.com>
2023-07-14 12:48:07 -0400

    try again

--- DNANexus/kernell_variance_component_aou.R
+++ DNANexus/kernell_variance_component_aou.R
@@ -1,8 +1,3 @@
-source("UKBB_200KWES_CVD/GENESIS_adaptation_source.R")
-source("TOPMed_AFib_pipeline/DNANexus/kernell_variance_component_modfied.R")
-source("TOPMed_AFib_pipeline/DNANexus/ExtractKernelStatistics_error_fixed.R")
-cat('\nReading in packages for analysis...\n')
-
 args=(commandArgs(TRUE))
 gdsfile=as.character(args[1])
 groupfile=as.character(args[2])
@@ -10,10 +5,17 @@ phenfile=as.character(args[3])
 ID_col=as.character(args[4])
 nullfile=as.character(args[5])
 outfile=as.character(args[6])
-testtype=as.character(args[7])
-afcutoff=as.numeric(args[8])
 
-kernell_variance_component_v3(gdsfile=gdsfile,groupfile=groupfile,phenfile=phenfile,ID_col=ID_col,nullfile=nullfile,outfile=outfile, test="ExtractKernelStatistics", vc.test=testtype, AF.max=afcutoff, MAC.max=Inf,use.weights=FALSE)
+##########################################################
+#### RUN hclof_noflag_missense0.9 ExtractKernelStatistics
+##########################################################
+
+cat('\nReading in packages for analysis...\n')
+source("UKBB_200KWES_CVD/GENESIS_adaptation_source.R")
+source("TOPMed_AFib_pipeline/DNANexus/kernell_variance_component_modfied.R")
+source("TOPMed_AFib_pipeline/DNANexus/ExtractKernelStatistics_error_fixed.R")
+
+kernell_variance_component_v2(gdsfile=gdsfile,groupfile=groupfile,phenfile=phenfile,ID_col=ID_col,nullfile=nullfile,outfile=outfile, test="ExtractKernelStatistics", vc.test="Score", AF.max=0.001, MAC.max=Inf,use.weights=FALSE)
 
 sessionInfo()
 quit("no")
